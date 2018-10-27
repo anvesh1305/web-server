e34fca1d2da562b39c40e1f0ff4a8a881465add1
Anvesh Chintu <Anvesh1305@gmail.com>
2018-10-27 14:00:43 -0400

    Update language

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var middleware = require('./middleware.js');
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
-    res.send("About Us");
+    res.send("About Us!");
 });
 
 //console.log(__dirname);
