5ab2e1ee6834957ae5295110eaaffe42c9a14df0
Anvesh Chintu <Anvesh1305@gmail.com>
2018-10-27 13:58:50 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3030;
+
+var middleware = require('./middleware.js');
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+    res.send("About Us");
+});
+
+//console.log(__dirname);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+    console.log('Express Server started at: ' + port);
+});
